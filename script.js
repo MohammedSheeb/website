42c8c9e6b7fd0aa153519ba8e8c5710a81eb91b5
mohammedsheeb1-oss <mohammedsheeb1@gmail.com>
2026-04-10 16:08:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ async function loadDiscord() {
 
     const name = user.global_name ? user.global_name : user.username;
     document.getElementById("displayName").innerText = name;
-    document.getElementById("tag").innerText = user.username;
+    document.getElementById("tag").innerText = "@" + user.username;
     document.getElementById("dot").className = "status-dot " + status;
+    document.getElementById("statusMiniDot").className = "mini-dot " + status;
+
+    document.getElementById("statusText").innerText =
+      status === "online" ? "Online" :
+      status === "idle" ? "Idle" :
+      status === "dnd" ? "Do Not Disturb" : "Offline";
   } catch (e) {
     console.error("Failed to load Discord data");
   }
