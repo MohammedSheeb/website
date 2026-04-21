7040c21c7887ab5eb8489e3c155ea3ae3eec5451
Mohammed Sheeb <mohammedsheeb1@gmail.com>
2026-04-21 13:59:17 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  loadDiscord();
+  connectLanyard();
 
+  const yearEl = document.getElementById("year");
+  if (yearEl) {
+    yearEl.textContent = new Date().getFullYear();
+  }
+});
