c40606f135fe93886e8c8c06f068078c8d5123f8
Mohammed Sheeb <mohammedsheeb1@gmail.com>
2026-04-24 01:14:06 +0300

    Delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  loadDiscord();
-  connectLanyard();
-
-  const yearEl = document.getElementById("year");
-  if (yearEl) {
-    yearEl.textContent = new Date().getFullYear();
-  }
-});
